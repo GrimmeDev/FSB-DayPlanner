c9ebde9b0814a0cf1cd496d4429056b2971460f4
Robert Grimes <rjgrimes@gmail.com>
2020-10-01 17:39:32 -0500

    Added additional comments for clarity of different items

--- Develop/mainScript.js
+++ Develop/mainScript.js
@@ -1,58 +1,57 @@
 //#region Variable Declaration
+// Stores current day ID in HTML
 var curDay = $("#currentDay");
+// stores container to display Hour, Task Field, and Save Button
 var timeContainer = $(".container");
-// finds reference to storedTasks, if non exists, create empty array
-// var tasksToDo = JSON.parse(localStorage.getItem("storedTasks")) || [];
-// creates variable to store current time ID, if it doesn't exist already
-// tasks.time = JSON.parse(localStorage.getItem("taskID"));
+// Empty variable used later to store user input tasks
 var task = "";
-// var tasks = {
-//     time: 0,
-//     task: ""
-// };
-//#endregion
 
 //#region Functioon Definitions
 // gets current day on page ready
 function getCurDay() {
     curDay.text(moment().format("dddd, MMMM Do"));
-    // displays as "Day, Month D#"
+    // displays as "Day, Month Day#"
 };
 
 // will display hour blocks for standard business hours (9am-5pm)
-// total of 9 blocks(?)
 function displayHours() {
-    var color = 0;
     for (var i = 0; i < 9; i++) {
         // variable delcarations
         // creates new row for each time
+        // Uses Bootstrap Icon of Text-Center as save button Icon
         var saveIcon = $('<svg width="75%" height="75%" viewBox="0 0 16 16" class="bi bi-text-center" fill="currentColor" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4 12.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm2-3a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zm-2-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/></svg>')
+        // creates an empty row with some bootstrap styling
         var newRow = $('<div class="row border">');
+        // sets the row to have an attribute of ID based on military time starting at 9
+        // ends at at 17
         newRow.attr("id", (i + 9));
+        // stores the ID attribute to variable used later to compare if localStorage
+        // key and value already exists
         var timeID = newRow.attr("id");
-        // var timeID = localStorage.setItem(newRow.attr("id"), "");
-        // console.log(newRow.attr("id"));
 
         // left column to display business hours
         var leftCol = $('<div class="col-lg-1 border text-right p-1 hour">');
         // mid column for text entry of task
         // color coded based on if time has passed, is currently, or in future
         var midCol = $('<textarea class="col-lg-10 border p-1 timeColor">');
-        // button with save icon to save current(?)/all text fields
+        // button with save icon to save all text fields
         var rightCol = $('<div class="col-lg-1 border d-flex align-items-stretch p-1 saveBtn">');
 
-        // base text adding
+        // base text adding for hour display
         if (newRow.attr("id") < 12)
             leftCol.text((i + 9) + " AM");
         else if (newRow.attr("id") == 12)
             leftCol.text((i + 9) + " PM");
         else
             leftCol.text((i + 9) - 12 + " PM");
+        // checks if localStorage key:value is empty
         if (localStorage.getItem(timeID) != "") {
             // loads previously saved text
+            // console.log(localStorage.getItem(timeID));
             midCol.text(localStorage.getItem(timeID));
         }
         else
+            // Should create placeholder text if no ID has been found with a value
             midCol.attr("placeholder", "Enter Text Here...");
         // rightCol.text("Save Button");
 
@@ -65,6 +64,7 @@ function displayHours() {
     };
 
     // color coding
+    // changes class to auto-style based on if the hour has passed, is current, or future
     function updateTime() {
         var currentTime = moment().hours();
         // console.log("Moment Time: " + currentTime);
@@ -72,16 +72,20 @@ function displayHours() {
             var curHour = parseInt($(this).parent().attr("id"));
             // console.log("Row Time: " + curHour);
 
+            // if the hour of the row is ahead of the current time
             if (curHour > currentTime)
                 $(this).addClass("future");
+            // if the hour of the row is equal to the current time
             else if (curHour == currentTime)
                 $(this).addClass("present");
+            // if the hour of the row is behind the current time
             else
                 $(this).addClass("past ");
         })
 
     };
 
+    // Sets ALL saveButton classes to perform the same function
     $(".saveBtn").click(function () {
         // console.log("Button clicked.");
         // console.log($(this));
@@ -99,21 +103,10 @@ function displayHours() {
             // tasks.time is the localStorage reference
             // push only tasks.task to localStorage based on the reference
             localStorage.setItem($(this).parent().attr("id"), task);
-
-
-            // tasks.time = $(this).parent().attr("id");
-            // console.log(tasks.time);
-            // tasks.task = $(this).val();
-            // console.log(tasks.task);
-            // localStorage.setItem("taskID", JSON.stringify(tasks));
         })
-        // tasks.time = $(this).parent().attr("id");
-        // console.log(tasks.time);
-        // tasks.task = $(this).val();
-        // console.log(tasks.task);
-        // tasksToDo.push(tasks);
-        // localStorage.setItem("storedTasks", JSON.stringify(tasksToDo));
     };
+
+    // Once script loads automatically displays the Day and Month at the top of the screen
     updateTime();
 };
 
